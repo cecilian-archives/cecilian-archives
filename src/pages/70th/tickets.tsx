7f276fc3ce3d83e3ad5fcba0cd3872e1fd9e1b6d
Ben Galloway <git@bengalloway.io>
2022-09-07 20:16:52 +0100

    Add accommodation discount link

--- src/pages/70th/tickets.tsx
+++ src/pages/70th/tickets.tsx
@@ -2,6 +2,9 @@ import type { ReactElement } from "react";
 import type { NextPageWithLayout } from "src/pages/_app";
 import Layout70th from "src/components/70th/Layout70th";
 
+import ButtonLink from "src/components/ButtonLink";
+import StarDivider from "src/components/StarDivider";
+
 const Tickets70th: NextPageWithLayout = () => {
   return (
     <section className="w-full bg-archiveBlue-50 p-8 flex flex-col items-center md:max-w-full md:justify-between">
@@ -27,6 +30,34 @@ const Tickets70th: NextPageWithLayout = () => {
         <p className="py-4 text-lg">
           Plus ones are more than welcome at each event, and especially so at the dinner.
         </p>
+
+        <StarDivider bgClass="bg-archiveBlue-50" />
+
+        <h3 className="text-gray-900 text-3xl pt-4 pb-2">Accommodation</h3>
+        <p className="py-4 text-lg">
+          If you've moved away from the area and would like to make a head start on where to stay
+          for your trip, our venue for Sunday's dinner, the{" "}
+          <a
+            href="https://www.gghotel.co.uk/"
+            target="_blank"
+            className="underline text-archiveBlue-600 hover:text-archiveBlue-400"
+          >
+            Glasgow Grosvenor Hotel
+          </a>
+          , have offered us a <b>10% discount</b> on their bed and breakfast advance purchase rate.
+        </p>
+        <ButtonLink
+          to="https://direct-book.com/properties/glasgowgrosvenor?locale=en&items[0][adults]=2&items[0][children]=0&items[0][infants]=0&currency=GBP&checkInDate=2022-10-21&checkOutDate=2022-10-24&promocode=GRP_EVE&trackPage=no"
+          target="_blank"
+        >
+          Claim the Discount
+        </ButtonLink>
+        <p className="py-4 text-lg">
+          To take advantage of this rate, simply book using the button above at least 21 days before
+          arrival.
+        </p>
+
+        <StarDivider bgClass="bg-archiveBlue-50" />
       </div>
       <a href="/images/Popstars.jpg" target="_blank">
         <img
